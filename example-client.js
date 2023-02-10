71330c32f8236da6ca31a113853778d343935890
anhvo31 <anhtuyet.vo3112@gmail.com>
2023-02-09 23:52:44 -0600

    Added exception handling to server and example client

--- example-client.js
+++ example-client.js
@@ -1,16 +1,16 @@
 /* 
-Client is for testing purposes
-Install the package: npm install zeromq@6.0.0-beta.16
-Note: zeroMQ version 5 doesn't have prebuilt binaries
+Client is for testing purposes.
+Server for company search uses ZeroMQ.
+As such, this test client is written in Node.JS using ZeroMQ.
+Install the ZeroMQ package: npm install zeromq@6.0.0-beta.16
 */
 
-const zmq = require("zeromq");
-
-async function runClient() {
+async function runCompanySearch() {
     /*
     Sends a request containing the string in the variable userInput to the server at port 3000.
     Converts response to a JSON.
     */
+    const zmq = require("zeromq");
     const PORT = 'tcp://localhost:3000';
 
     // Creates socket and connect to port
@@ -19,22 +19,33 @@ async function runClient() {
 
     // Variable to hold message sent to the server
     // Example used here is to receive data on a clown company called "Starlights"
-    let userInput = "Starlights"
+    let userInput = "Starlights";
 
-    console.log("Sending ", userInput)
+    console.log("Sending", userInput);
     await socket.send(userInput);
 
     const [result] = await socket.receive();
-
-    // Converts result(byte literal) to JSON
     const result_string = result.toString();
-    const result_JSON = JSON.parse(result_string)
 
-    console.log("Received: ", result_JSON);
+    try {
+        console.log("Received: ", result_string);
+        const result_JSON = JSON.parse(result_string)
 
-    // Example on how to extract info from JSON
-    console.log(result_JSON["Price"])
+        /* 
+        Example on how to extract info from JSON:
+            let price = result_JSON["Price"];
+            console.log(price);
+        */
 
+    } catch (SyntaxError) {
+        console.log(result_string);
+    }
 }
 
-runClient()
+// How to call the function
+runCompanySearch()
+
+/* 
+Example on how to export the function for use in other files:
+    exports.runCompanySearch = runCompanySearch;
+*/
--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import time
 
 PORT = "tcp://*:3000"
 
-# creates the socket object and binds it to a port
+# Creates the socket object and binds it to a port
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind(PORT)
@@ -14,7 +14,7 @@ socket.bind(PORT)
 while True:
     print("Waiting for request...")
     
-    # receives the message sent and converts it to a string
+    # Receives the message sent and converts it to a string
     request = socket.recv()
     request = request.decode('utf-8')
     print(f"Received: {request}")
@@ -23,27 +23,32 @@ while True:
 
     user_request = request
 
-    # uses dataframe to access data in excel file
+    # Uses dataframe to access data in excel file
     excel_path = "clown_companies.csv"
     df = pd.read_csv(excel_path)
 
-    # determines index of the company in the request
-    companyList = [x for x in df['Company Name']]
-    index = companyList.index(user_request)
+    company_list = [x for x in df['Company Name']]
 
-    # extracts data about the company
-    name = df['Company Name'][index]
-    price = df['Price (per hour)'][index]
-    min = df['Min Hours'][index]
-    max = df['Max Hours'][index]
+    try:
+        # Determines index of the company in the request
+        index = company_list.index(user_request)
+    except ValueError:
+        print("Company does not exist")
+        socket.send(b'Company does not exist')
+        continue
+    except KeyboardInterrupt:
+        print("Closing socket...")
+        break
+    else:
+        # Extracts data about the company
+        name = df['Company Name'][index]
+        price = df['Price (per hour)'][index]
+        min_hours = df['Min Hours'][index]
+        max_hours = df['Max Hours'][index]
 
-    # appends data to dictionary object
-    result = {}
-    result["Name"] = f"{name}"
-    result["Price"] = f"{price}"
-    result["Min Hours"] = f"{min}"
-    result["Max Hours"] = f"{max}"
+        # Appends data to dictionary object
+        result = {"Name": f"{name}", "Price": f"{price}", "Min Hours": f"{min_hours}", "Max Hours": f"{max_hours}"}
 
-    # sends dictionary as JSON reply back to client
-    print(f"Sending: {result}")
-    socket.send_json(result)
+        # Sends dictionary as JSON reply back to client
+        print(f"Sending: {result}")
+        socket.send_json(result)
